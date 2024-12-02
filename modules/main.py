d1afc9faf7d46cb8c9605b11d25ab6ffbd4bfdfa
ROWDY <excuseboy13@gmail.com>
2024-12-02 16:05:40 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -22,7 +22,7 @@ from bs4 import BeautifulSoup
 
 bot = Client("bot",
              bot_token="7838011708:AAGuLm8D0YtQcnJqExCBpQ-DD6InJikO2tM" 
-             api_id=27679306,
+             api_id=27679306
              api_hash=d0f390f798eec4ddb9246a00a44b7a3f)
 
 
